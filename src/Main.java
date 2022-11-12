1f822fe4468b1f636881f4b41ca45d0ed9edf74e
Prashik Kamble <kambleprashik7349@gmail.com>
2022-11-12 15:42:04 +0530

    ADDED : code to print elements of an array.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = {1,2,3,4};
+        PrintElementsOfArray eleObj = new PrintElementsOfArray();
+        eleObj.elementsOfArray(arr);
     }
 }
---
+++ src/PrintElementsOfArray.java
@@ -0,0 +1,7 @@
+public class PrintElementsOfArray {
+    public void elementsOfArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
