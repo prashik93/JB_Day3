8fdc1d524d709cc335110f07600a74174890c857
Prashik Kamble <kambleprashik7349@gmail.com>
2022-11-13 17:39:02 +0530

    MODIFIED : main.java remove comment from code

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,40 @@
 public class Main {
     public static void main(String[] args) {
-//        int[] arr = {1,2,3,4};
-//        PrintElementsOfArray eleObj = new PrintElementsOfArray();
-//        eleObj.elementsOfArray(arr);
-//
-//        int[] arr2 = {1,1,3,2,2,3,4,5,6,5,7,8,7};
-//        FrequencyOfEachElements freObj = new FrequencyOfEachElements();
-//        freObj.frequencyOfEachElements(arr2);
-//
-//        int[] arr3 = {25,24,33,14,5,28};
-//        LargestElement larObj = new LargestElement();
-//        larObj.largestElement(arr3);
-//
-//        int[] arr4 = {-1,5,0,8,34};
-//        SmallestElement smaObj = new SmallestElement();
-//        smaObj.smallestElement(arr4);
-//
-//        int[] arr5 = {1,2,4,4,5,6,10,8,9};
-//        EvenPositionElement evenObj = new EvenPositionElement();
-//        evenObj.evenPositionElement(arr5);
-//
-//        int[] arr6 = {1,2,3,4,10,6,7,8,9};
-//        ReverseElementsOfArray revObj = new ReverseElementsOfArray();
-//        revObj.reverseElementsOfArray(arr6);
-//
-//        int[] arr7 = {1,2,3,4,3,5,8,9,7,8};
-//        OddPositionElement oddObj = new OddPositionElement();
-//        oddObj.oddPositionElement(arr7);
-//
-//        int[] arr8 = {1,2,3,4,5,4,3,2,5};
-//        DuplicateElements dupObj = new DuplicateElements();
-//        dupObj.duplicateElements(arr8);
-//
-//        int[] arr9 = {2,3,5,7,8,1,9,4,6};
-//        AscendingAnArray ascObj = new AscendingAnArray();
-//        ascObj.ascendingAnArray(arr9);
+        int[] arr = {1,2,3,4};
+        PrintElementsOfArray eleObj = new PrintElementsOfArray();
+        eleObj.elementsOfArray(arr);
+
+        int[] arr2 = {1,1,3,2,2,3,4,5,6,5,7,8,7};
+        FrequencyOfEachElements freObj = new FrequencyOfEachElements();
+        freObj.frequencyOfEachElements(arr2);
+
+        int[] arr3 = {25,24,33,14,5,28};
+        LargestElement larObj = new LargestElement();
+        larObj.largestElement(arr3);
+
+        int[] arr4 = {-1,5,0,8,34};
+        SmallestElement smaObj = new SmallestElement();
+        smaObj.smallestElement(arr4);
+
+        int[] arr5 = {1,2,4,4,5,6,10,8,9};
+        EvenPositionElement evenObj = new EvenPositionElement();
+        evenObj.evenPositionElement(arr5);
+
+        int[] arr6 = {1,2,3,4,10,6,7,8,9};
+        ReverseElementsOfArray revObj = new ReverseElementsOfArray();
+        revObj.reverseElementsOfArray(arr6);
+
+        int[] arr7 = {1,2,3,4,3,5,8,9,7,8};
+        OddPositionElement oddObj = new OddPositionElement();
+        oddObj.oddPositionElement(arr7);
+
+        int[] arr8 = {1,2,3,4,5,4,3,2,5};
+        DuplicateElements dupObj = new DuplicateElements();
+        dupObj.duplicateElements(arr8);
+
+        int[] arr9 = {2,3,5,7,8,1,9,4,6};
+        AscendingAnArray ascObj = new AscendingAnArray();
+        ascObj.ascendingAnArray(arr9);
 
         int[] arr10 = {20,4,7,9,13,21,8,10};
         SecondLargestNumber secObj = new SecondLargestNumber();
